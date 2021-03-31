228daf469fd70cf19cfea27f70678dd4d0b980dd
squareyun <squareyun@gmail.com>
2021-04-01 01:52:51 +0900

    typedef token

---
+++ scanner/scan.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXRESERVED 6
+
+typedef enum {
+	/* book-keeping tokens */
+	ERROR,
+	/* reserved words */
+	ELSE, IF, INT, RETURN, VOID, WHILE,
+	/* special symbols */
+	PLUS, MINUS, MUL, DIV, LT, LE, GT, GE, EQ, NE, ASSIGN, SEMI, COMMA,
+	LPAREN, RPAREN, LSQUARE, RSQUARE, LCURLY, RCURLY, LCOMMENT, RCOMMENT,
+	/* multicharacter tokens */
+	ID, NUM
+} TokenType;
+
+/* reserved words talbe */
+struct {
+	char* str;
+	TokenType tok;
+} reservedWords[MAXRESERVED]
+= { {"else",ELSE},{"if",IF},{"int",INT},{"return",RETURN},{"void",VOID},{"while",WHILE} };
+
+/* lookup if identifier is reserved word */
+TokenType reservedLookup(char* s) {
+	int i;
+	for (i = 0; i < MAXRESERVED; i++)
+		if (!strcmp(s, reservedWords[i].str))
+			return reservedWords[i].tok;
+	return ID;
+}
+
+void main() {
+}
