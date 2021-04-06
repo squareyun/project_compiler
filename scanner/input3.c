0d58d8d17913c78320dd075987ddc4cd99116c10
squareyun <squareyun@gmail.com>
2021-04-06 23:27:56 +0900

    Test: Add test case

---
+++ scanner/input3.c
@@ -0,0 +1,29 @@
+x+y
+x  +  y
+
+xyz=3
+xyz    =    3
+
+abc     xyz
+if    return
+abc     1234
+561     1234
+>    =
+
+xyz!=10/*comment*/
+xyz!/*comment*/=10
+
+
+111  aaa
+111#aaa
+111aaa
+
+aaa111
+aaa#111
+aaa  111
+
+
+/   *comment*/
+
+/*   comment  *   /
+a=1
